e97b232d2175c448c12adf5ffbf7357a0a26a4e9
Katerina Prastakou <kate.prast@gmail.com>
2016-07-11 16:12:24 -0400

    clean up + timestamp

--- graphData.py
+++ graphData.py
@@ -6,7 +6,7 @@ import numpy as np
 def findOccurences(s, ch):
     return [i for i, letter in enumerate(s) if letter == ch]
     
-with open("/Users/kat/Documents/XCODE/graphPython/dataNotMoving2.txt", "r") as f:
+with open("/Users/kat/Documents/XCODE/graphPython/dataNewInterval.txt", "r") as f:
     data = f.read()
 
 data = data.split('\n')
@@ -28,7 +28,7 @@ for i in range(0,len(updateData['x'])):
     updateData['x'][i] = float(updateData['x'][i])
     updateData['y'][i] = float(updateData['y'][i])
     updateData['z'][i] = float(updateData['z'][i])
-    
+
 #for i in range(1,len(updateData['x'])):
 #    if (abs(float(updateData['x'][i]) - float(updateData['x'][i-1])) < .05):
 #        updateData['x'][i] = updateData['x'][i-1]
@@ -41,22 +41,23 @@ medianValues = []
 for i in range(5, len(updateData['z'])-5):
     items = updateData['z'][i-5:i+5]
     medianValues.append(np.median(items))
-#    items = updateData['x'][i:i+5])
-#    print numpy.median(items)
 
+start = dt.datetime.fromtimestamp(float(data[0][11:23][6:]))
+end = dt.datetime.fromtimestamp(float(data[len(data)-1][11:23][6:]))
+interval = int(str(end)[17:19]) - int(str(start)[17:19])
 
-
+sec = updateData['timestamp'][0]
+for item in range(0,interval):
+    plt.axvline(x=sec, color='k', linestyle='-')
+    sec += dt.timedelta(0,1)
 
 #print len(updateData['timestamp'][5:len(updateData['x'])-5])
-#print len(medianValues)        
+      
 plt.plot(updateData['timestamp'],updateData['x'])
-    
-    
 plt.plot(updateData['timestamp'],updateData['y'])
-plt.plot(updateData['timestamp'][5:len(updateData['z'])-5],medianValues)
+plt.plot(updateData['timestamp'],updateData['z'])
 
-#print updateData['timestamp']
-with open("/Users/kat/Documents/XCODE/graphPython/touch_data2.txt", "r") as f:
+with open("/Users/kat/Documents/XCODE/graphPython/touchNewInterval.txt", "r") as f:
     data = f.read()
 
 data = data.split('\n')
@@ -65,23 +66,6 @@ touch = []
 for item in data:
     i = dt.datetime.fromtimestamp(float(item[11:23][6:]))
     ax.axvline(i, color = 'k', linestyle='--')
-
-
-#a = [1,2,3,4]
-#b = [17,12,11,10]
-#c = [-1,-4,5,9]
-#print map(lambda x,y:x+y, a,b)
-#
-#fib = [0,1,1,2,3,5,8,13,21,34,55]
-#result = filter(lambda x: x % 2, fib)
-#print result
-#
-#print reduce(lambda x,y:x-y, fib)
-#f = lambda x: b - a
-#print f
-##print filter(f, [47,11,42,102,13])
-#
-##print reduce(f, [47,11,42,102,13])
 
 plt.legend(['x', 'y', 'z', 'touch event'], loc='upper left')
 
