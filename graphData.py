c456c39f477e62f2b1381c5e2e26bd110fe40227
Katerina Prastakou <kate.prast@gmail.com>
2016-07-14 17:59:43 -0400

    bigger interval + low pass

--- graphData.py
+++ graphData.py
@@ -7,7 +7,7 @@ import matplotlib.transforms as mtransforms
 def findOccurences(s, ch):
     return [i for i, letter in enumerate(s) if letter == ch]
     
-with open("/Users/kat/Documents/XCODE/graphPython/highFreqData.txt", "r") as f:
+with open("/Users/kat/Documents/XCODE/graphPython/dataDavis.txt", "r") as f:
     data = f.read()
 
 data = data.split('\n')
@@ -30,25 +30,12 @@ for i in range(0,len(updateData['x'])):
     updateData['z'][i] = float(updateData['z'][i])
 
 
-lowpX = [None]*len(updateData['z'])
-lowpY = [None]*len(updateData['z'])
-lowpZ = [None]*len(updateData['z'])
-lowpX[0] = 0.0
-lowpY[0] = 0.0
-lowpZ[0] = 0.0
-
 filteredX = [None]*len(updateData['z'])
 filteredY = [None]*len(updateData['z'])
 filteredZ = [None]*len(updateData['z'])
 filteredX[0] = 0.0
 filteredY[0] = 0.0
 filteredZ[0] = 0.0
-
-#for i in range(1,len(updateData['z'])):
-#    #print filteredZ[i-1]
-#    lowpX[i] = lowpX[i-1] + 0.2 * (updateData['x'][i] - lowpX[i-1])
-#    lowpY[i] = lowpY[i-1] + 0.2 * (updateData['y'][i] - lowpY[i-1])
-#    lowpZ[i] = lowpZ[i-1] + 0.2 * (updateData['z'][i] - lowpZ[i-1])
 
 for i in range(1,len(updateData['z'])):
     filteredX[i] = 0.1 * (filteredX[i-1] + updateData['x'][i] - updateData['x'][i-1]) 
@@ -60,7 +47,6 @@ for i in range(1,len(updateData['z'])):
         filteredZ[i] = 0.0
 
 
-
 start = float(updateData['timestamp'][0])
 end = float(updateData['timestamp'][len(data)-1])
 interval = int(end - start)
@@ -68,7 +54,7 @@ interval = int(end - start)
 i = 0
 sec = updateData['timestamp'][0]
 
-with open("/Users/kat/Documents/XCODE/graphPython/highFreqTouch.txt", "r") as f:
+with open("/Users/kat/Documents/XCODE/graphPython/touchDavis.txt", "r") as f:
     touchData = f.read()
 
 touchData = touchData.split('\n')
@@ -76,31 +62,24 @@ touchData = touchData.split('\n')
 for item in touchData:   
     ax.axvline(float(item), color = 'k', linestyle='--')
 
-
 for item in range(0,interval/2):
     plt.axvline(x=sec, color='r', linestyle='-')
-    #if (i%2 == 0):
-        #ax.axvspan(sec, sec + 1, alpha = 0.5, color='red')
-        #pass
     time = sec
-    #print "time is " + str(time)
-    #print "sec is " + str(sec)
     color = 0
+    
     while time < (sec + 2):
         point =  np.interp(time, updateData['timestamp'], filteredZ)
         if point != 0:
             color += 1
+        #if abs(point) > 0.04:
+        #    color = -1
+        #    break
         time += .01
         
-    if (color != 0):
+    if (color > 0):
         right = 0;
         for touch in touchData:
-            #print type(touch)
-            #print type(sec)
-            #print "item is " + str(touch) + ", sec is " + str(sec) + ", sec + 1 is " + str(sec + 1)
-            #print touch > sec
-            #print touch < (sec + 1)
-            if (float(touch) > sec and float(touch )< (sec + 2)):
+            if (float(touch) > sec and float(touch ) < (sec + 2)):
                 right += 1
         if right > 0:    
             ax.axvspan(sec, sec + 2, alpha = 0.5, color='green') 
@@ -114,10 +93,7 @@ for item in range(0,interval/2):
 #plt.plot(updateData['timestamp'],filteredX)
 #plt.plot(updateData['timestamp'],filteredY)
 plt.plot(updateData['timestamp'],filteredZ)
-
-
-
-    
+   
 plt.legend(['z', 'touch event'], loc='upper left')
 plt.gcf().set_size_inches(18.5, 10.5, forward=True)
 plt.show()
