cf23752014ca38664b39fe6e8552c3c7b3f630f0
Katerina Prastakou <kate.prast@gmail.com>
2016-07-14 13:22:01 -0400

    high pass working

--- graphData.py
+++ graphData.py
@@ -7,7 +7,7 @@ import matplotlib.transforms as mtransforms
 def findOccurences(s, ch):
     return [i for i, letter in enumerate(s) if letter == ch]
     
-with open("/Users/kat/Documents/XCODE/graphPython/dataMoving.txt", "r") as f:
+with open("/Users/kat/Documents/XCODE/graphPython/dataWorking.txt", "r") as f:
     data = f.read()
 
 data = data.split('\n')
@@ -23,14 +23,25 @@ for item in data:
 ax = plt.gca()
 ax.set_color_cycle(['purple', 'green', 'blue', 'black'])
 
+
 for i in range(0,len(updateData['x'])):
     updateData['x'][i] = float(updateData['x'][i])
     updateData['y'][i] = float(updateData['y'][i])
     updateData['z'][i] = float(updateData['z'][i])
 
-#for i in range(0,len(updateData['z'])):
-#    if (abs(float(updateData['z'][i]) - float(updateData['z'][i-1])) < .02):
-#        updateData['z'][i] = -1.0;
+filteredX = [None]*len(updateData['z'])
+filteredY = [None]*len(updateData['z'])
+filteredZ = [None]*len(updateData['z'])
+filteredX[0] = 0.0
+filteredY[0] = 0.0
+filteredZ[0] = 0.0
+
+for i in range(1,len(updateData['z'])):
+    #print filteredZ[i-1]
+    filteredX[i] = 0.1 * (filteredX[i-1] + updateData['x'][i] - updateData['x'][i-1]) 
+    filteredY[i] = 0.1 * (filteredY[i-1] + updateData['y'][i] - updateData['y'][i-1]) 
+    filteredZ[i] = 0.1 * (filteredZ[i-1] + updateData['z'][i] - updateData['z'][i-1]) 
+
 
 start = float(updateData['timestamp'][0])
 end = float(updateData['timestamp'][len(data)-1])
@@ -39,26 +50,19 @@ interval = int(end - start)
 i = 0
 sec = updateData['timestamp'][0]
 for item in range(0,interval):
-    #plt.axvline(x=sec, color='r', linestyle='-')
+    plt.axvline(x=sec, color='r', linestyle='-')
     if (i%2 == 0):
         #ax.axvspan(sec, sec + 1, alpha = 0.5, color='red')
         pass
     sec += 1
     i+=1
 
-plt.plot(updateData['timestamp'],updateData['x'])
-plt.plot(updateData['timestamp'],updateData['y'])
-plt.plot(updateData['timestamp'],updateData['z'])
+#plt.plot(updateData['timestamp'],filteredX)
+#plt.plot(updateData['timestamp'],filteredY)
+plt.plot(updateData['timestamp'],filteredZ)
 
 
-for i in range(1,len(updateData['x'])-1):
-    print updateData['z'][i] - updateData['z'][i-1]
-    print updateData['x'][i] - updateData['x'][i-1]    
-    if abs(updateData['z'][i] - updateData['z'][i-1]) >4.5 *abs(updateData['x'][i] - updateData['x'][i-1]):
-       ax.axvline(updateData['timestamp'][i], color = 'r', linestyle='-')
-
-
-with open("/Users/kat/Documents/XCODE/graphPython/touchMoving.txt", "r") as f:
+with open("/Users/kat/Documents/XCODE/graphPython/touchWorking.txt", "r") as f:
     touchData = f.read()
 
 touchData = touchData.split('\n')
@@ -70,7 +74,3 @@ for item in touchData:
 plt.gcf().set_size_inches(18.5, 10.5, forward=True)
 plt.show()
 
-#for i in range(1,len(updateData['x'])):
-#    if (abs(float(updateData['x'][i]) - float(updateData['x'][i-1])) < .05):
-#        updateData['x'][i] = updateData['x'][i-1]
-#
