7f87fae860632598848ee391786176b33ffd9741
Katerina Prastakou <kate.prast@gmail.com>
2016-07-19 16:36:19 -0400

    changes to filtering

--- graphData.py
+++ graphData.py
@@ -7,82 +7,19 @@ import matplotlib.transforms as mtransforms
 def findOccurences(s, ch):
     return [i for i, letter in enumerate(s) if letter == ch]
     
-with open("/Users/kat/Documents/XCODE/graphPython/checkFinal.txt", "r") as f:
-    data = f.read()
-
-data = data.split('\n')
-updateData = {'timestamp':[], 'x':[], 'y':[], 'z':[]}
-
-for item in data:
-    m = findOccurences(item, "|")  
-    updateData['timestamp'].append(float(item[:m[0]]))
-    updateData['x'].append(item[m[0]+1:m[1]])
-    updateData['y'].append(item[m[1]+1:m[2]])
-    updateData['z'].append(item[m[2]+1:])
-
-ax = plt.gca()
-ax.set_color_cycle(['purple','black'])
-
-
-for i in range(0,len(updateData['x'])):
-    updateData['x'][i] = float(updateData['x'][i])
-    updateData['y'][i] = float(updateData['y'][i])
-    updateData['z'][i] = float(updateData['z'][i])
-
-
-filteredX = [None]*len(updateData['z'])
-filteredY = [None]*len(updateData['z'])
-filteredZ = [None]*len(updateData['z'])
-filteredX[0] = 0.0
-filteredY[0] = 0.0
-filteredZ[0] = 0.0
-
-for i in range(1,len(updateData['z'])):
-    filteredX[i] = 0.1 * (filteredX[i-1] + updateData['x'][i] - updateData['x'][i-1]) 
-    filteredY[i] = 0.1 * (filteredY[i-1] + updateData['y'][i] - updateData['y'][i-1]) 
-    filteredZ[i] = 0.1 * (filteredZ[i-1] + updateData['z'][i] - updateData['z'][i-1]) 
-
-for i in range(1,len(updateData['z'])):
-    if (abs(filteredZ[i] - filteredZ[i-1]) < .009):
-        filteredZ[i] = 0.0
-
-
-start = float(updateData['timestamp'][0])
-end = float(updateData['timestamp'][len(data)-1])
-interval = int(end - start)
-
-i = 0
-sec = updateData['timestamp'][0]
-
-with open("/Users/kat/Desktop/touchData.txt", "r") as f:
-    touchData = f.read()
-
-touchData = touchData.split(' ')
-
-for i in range (0,len(touchData)-1): 
-    ax.axvline(float(touchData[i]), color = 'k', linestyle='--')
-for item in range(0,interval/2 + 1):
-    plt.axvline(x=sec, color='r', linestyle='-')
-    time = sec
-    color = 0
     
-    while time < (sec + 2):
-        point =  np.interp(time, updateData['timestamp'], filteredZ)
-        if point != 0:
-            color += 1
-        if abs(point) > 0.05:
-            color = -1
-            break
-        time += .002
-    
+def analysis(num, touchData, sec, color):
     right = 0
     wrong = 0
-    if (color > 0):
+    if (color > num):
+        #print sec
+        #print color
         for x in range(0, len(touchData)-1):
             if (float(touchData[x]) > sec and float(touchData[x] ) < (sec + 2)):
                 right += 1
         if right > 0:    
             ax.axvspan(sec, sec + 2, alpha = 0.5, color='green') 
+            #print "option 1"
         else:
             ax.axvspan(sec, sec + 2, alpha = 0.5, color='red') 
     else:
@@ -93,14 +30,160 @@ for item in range(0,interval/2 + 1):
             ax.axvspan(sec, sec + 2, alpha = 0.5, color='red') 
         else:
             ax.axvspan(sec, sec + 2, alpha = 0.5, color='green') 
-                                        
-    i+=2
+            #print "option 2" 
+            #print num   
+            #print color    
+
+
+
+def filtering(updateData):
+    filteredZ = [None]*len(updateData['z'])
+    filteredZ[0] = 0.0
+
+    for i in range(1,len(updateData['z'])): 
+        filteredZ[i] = 0.1 * (filteredZ[i-1] + updateData['z'][i] - updateData['z'][i-1]) 
+        if (abs(filteredZ[i] - filteredZ[i-1]) < .009):
+            filteredZ[i] = 0.0
+    
+    return filteredZ
+
+        
+with open("/Users/kat/Desktop/tableAndNot.txt", "r") as f:
+    data = f.read()
+    
+
+data = data.split('\n')
+updateData = {'timestamp':[], 'x':[], 'y':[], 'z':[]}
+
+for item in data:
+    m = findOccurences(item, "|")  
+    updateData['timestamp'].append(float(item[:m[0]]))
+    updateData['z'].append(item[m[2]+1:])
+
+ax = plt.gca()
+ax.set_color_cycle(['purple','black'])
+
+
+for i in range(0,len(updateData['x'])):
+    updateData['z'][i] = float(updateData['z'][i])
+
+#
+
+start = float(updateData['timestamp'][0])
+end = float(updateData['timestamp'][len(data)-1])
+interval = int(end - start)
+
+i = 0
+sec = updateData['timestamp'][0]
+
+with open("/Users/kat/Desktop/touchDataTableAndNot.txt", "r") as f:
+    touchData = f.read()
+
+touchData = touchData.split(' ')
+
+for i in range (0,len(touchData)-1): 
+    ax.axvline(float(touchData[i]), color = 'k', linestyle='--')
+
+
+avgs = []
+secs = []
+colors = []
+for item in range(0,interval/2 + 1):
+    plt.axvline(x=sec, color='r', linestyle='-')
+    time = sec
+    color = 0
+    avg = 0
+    m = 0
+    #print sec
+    while time < (sec + 2):
+        avg += np.interp(time, updateData['timestamp'], updateData['z'])
+        time += .002
+        m += 1
+    avg /= m
+    avgs.append(avg)
+    secs.append(sec)                                                 
+    #i+=2
     sec += 2
     
+for n in range(0,len(avgs)-1):
+    if i == 0:
+        if abs(avgs[n] - avgs[n+1]) < 0.015:
+            
+            analysis(0, touchData, secs[n], colors[n])
+        else:
+            #print "old av: " + str(avgs[x]) + ", new av: " + str(avgs[x+1])
+            analysis(64, touchData, secs[n], colors[n])    
+    elif n < len(avgs) - 1:
+        if abs(avgs[n] - avgs[n+1]) < 0.015 or abs(avgs[n-1] - avgs[n]) < 0.015:
+            #print "heyyaaaaaaaaa, old av: " + str(avgs[x]) + ", new av: " + str(avgs[x+1])
+            analysis(0, touchData, secs[n], colors[n])
+            #print x
+        else:
+            #print "old av: " + str(avgs[x]) + ", new av: " + str(avgs[x+1])
+            analysis(64, touchData, secs[n], colors[n]) 
+    else:
+        if abs(avgs[n] - avgs[n-1]) < 0.015:
+            #print "heyy, old av: " + str(avgs[x]) + ", new av: " + str(avgs[x+1])
+            analysis(0, touchData, secs[n], colors[n])
+        else:
+            #print "old av: " + str(avgs[x]) + ", new av: " + str(avgs[x+1])
+            analysis(64, touchData, secs[n], colors[n])
+                
+
+
+
+for item in range(0,interval/2 + 1):
+    time = sec
+    color = 0
+    avg = 0
+    m = 0
+    #print sec
+    while time < (sec + 2):
+        point =  np.interp(time, updateData['timestamp'], filteredZ)
+        avg += np.interp(time, updateData['timestamp'], updateData['z'])
+        if point != 0:
+            color += 1
+        if abs(point) > 0.07:
+            color = -1
+            break
+        time += .002
+        m += 1    
+        
+            
+                
+                    
+                            
+for x in range(0,len(avgs)-1):
+    if i == 0:
+        
+        if abs(avgs[x] - avgs[x+1]) < 0.015:
+            #print "heyy, old av: " + str(avgs[x]) + ", new av: " + str(avgs[x+1])
+            analysis(0, touchData, secs[x], colors[x])
+        else:
+            #print "old av: " + str(avgs[x]) + ", new av: " + str(avgs[x+1])
+            analysis(64, touchData, secs[x], colors[x])    
+    elif x < len(avgs) - 1:
+        if abs(avgs[x] - avgs[x+1]) < 0.015 or abs(avgs[x-1] - avgs[x]) < 0.015:
+            #print "heyyaaaaaaaaa, old av: " + str(avgs[x]) + ", new av: " + str(avgs[x+1])
+            analysis(0, touchData, secs[x], colors[x])
+            #print x
+        else:
+            #print "old av: " + str(avgs[x]) + ", new av: " + str(avgs[x+1])
+            analysis(64, touchData, secs[x], colors[x]) 
+    else:
+        if abs(avgs[x] - avgs[x-1]) < 0.015:
+            #print "heyy, old av: " + str(avgs[x]) + ", new av: " + str(avgs[x+1])
+            analysis(0, touchData, secs[x], colors[x])
+        else:
+            #print "old av: " + str(avgs[x]) + ", new av: " + str(avgs[x+1])
+            analysis(64, touchData, secs[x], colors[x])
+                
 #plt.plot(updateData['timestamp'],filteredX)
 #plt.plot(updateData['timestamp'],filteredY)
 plt.plot(updateData['timestamp'],filteredZ)
-   
+#plt.plot(updateData['timestamp'],updateData['z'])
+
+      
 plt.legend(['z', 'touch event'], loc='upper left')
 plt.gcf().set_size_inches(18.5, 10.5, forward=True)
 plt.show()
